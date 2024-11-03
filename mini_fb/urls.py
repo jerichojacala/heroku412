6f9a2591dc579f0acefd195828a1b777077070a5
jerichojacala <jjacala@bu.edu>
2024-11-03 11:33:49 -0500

    added logout confirmation

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path(r'profile/friend_suggestions',views.ShowFriendSuggestionsView.as_view(),name="friend_suggestions"),    
     path(r'profile/news_feed',views.ShowNewsFeedView.as_view(),name="news_feed"),
     path('login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(next_page='show_all_profiles'), name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name="logout"),
 ]
